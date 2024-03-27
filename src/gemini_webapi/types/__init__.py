d3a61214bd940d6c16fd5b74585a2502dfa35657
muxueqz <zhangmingyuan240@gmail.com>
2024-03-27 15:40:49 +0800

    rename chats to conversation

--- src/gemini_webapi/types/__init__.py
+++ src/gemini_webapi/types/__init__.py
@@ -1,3 +1,4 @@
 from .image import Image, WebImage, GeneratedImage  # noqa: F401
 from .candidate import Candidate  # noqa: F401
 from .modeloutput import ModelOutput  # noqa: F401
+from .conversation import Conversation # noqa: F401
--- src/gemini_webapi/types/conversation.py
+++ src/gemini_webapi/types/conversation.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 
-class Chat(BaseModel):
+class Conversation(BaseModel):
     """
     A single reply candidate object in the model output. A full response from Gemini usually contains multiple reply candidates.
 
@@ -14,14 +14,3 @@ class Chat(BaseModel):
 
     cid: str
     name: str
-
-class Chats(BaseModel):
-    """
-    A single reply candidate object in the model output. A full response from Gemini usually contains multiple reply candidates.
-
-    Parameters
-    ----------
-    Chats: `list[Chat]`
-    """
-
-    chats: list[Chat]
